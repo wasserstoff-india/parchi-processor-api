1cac93030ad8419aef166ab44d7654159f5e95d7
ayush <tripathiayush600@gmail.com>
2023-07-31 12:50:01 +0530

    changes

--- pdfToText/service/Excel.js
+++ pdfToText/service/Excel.js
@@ -1,88 +1,90 @@
 export const filterRows = (rows, columnFilters) => {
-    if (
-      columnFilters.includes('>') ||
-      columnFilters.includes('<') ||
-      columnFilters.includes('=')
-    ) {
-      return filteredRowsWithOperator(rows, columnFilters);
-    } else {
-      return filteredRowsSimple(rows, columnFilters);
-    }
-  };
-  
-  export const filteredRowsSimple = (rows, columnFilters) => {
-    const filters = columnFilters.split(/ OR | or /);
-    const selectedRows = [];
-  
-    filters.forEach((filter) => {
-      const [columnName, value] = filter.replace(/'/g, '').split('=');
+  const hasOperator =
+    columnFilters.includes('>') || columnFilters.includes('<');
+
+  if (hasOperator) {
+    return filteredRowsWithOperator(rows, columnFilters);
+  } else {
+    return filteredRowsSimple(rows, columnFilters);
+  }
+};
+
+export const filteredRowsSimple = (rows, columnFilters) => {
+  const filters = columnFilters.split(/[Oo][Rr]/);
+  const selectedRows = [];
+
+  filters.forEach((filter) => {
+    // Use regex to remove extra spaces around the equal sign
+    const [filterColumnValue, filterRowValue] = filter
+      .replace(/'/g, '')
+      .split(/\s*=\s*/); // Split on '=' with optional spaces
+    const trimmedColumnName = filterColumnValue.trim().toLowerCase();
+    console.log(trimmedColumnName, 'trimmedColumnName simple value');
+    const trimmedRowValue = filterRowValue.trim().toLowerCase();
+    console.log(trimmedRowValue, 'trimmed row value');
+
+    const filteredRows = rows.filter((row) => {
+      const columnValue = row[trimmedColumnName]?.trim().toLowerCase();
+      return columnValue === trimmedRowValue;
+    });
+
+    selectedRows.push(...filteredRows);
+  });
+
+  return selectedRows;
+};
+
+const filteredRowsWithOperator = (rows, columnFilters) => {
+  const filters = columnFilters.split(/ OR | or /);
+  const selectedRows = [];
+
+  filters.forEach((filter) => {
+    const conditions = filter.split(/ AND | and /);
+    let conditionMatches = [];
+
+    conditions.forEach((condition) => {
+      const [columnName, filterValue] = condition
+        .replace(/'/g, '')
+        .split(/[><=]/);
+      const operator = condition.replace(/[^><]/g, '').trim();
       const trimmedColumnName = columnName.trim().toLowerCase();
-      console.log(trimmedColumnName, 'trimmedColumnName  value');
-      const trimmedRowValue = value.trim().toLowerCase();
-      console.log(trimmedRowValue, 'trimmmed row value');
-  
+      console.log(trimmedColumnName, ':::trimmedColumnName operator  value ');
+      const trimmedRowValue = filterValue.trim().toLowerCase();
+
       const filteredRows = rows.filter((row) => {
-        const columnValue = row[trimmedColumnName].trim().toLowerCase();
-        return columnValue === trimmedRowValue;
-      });
-  
-      selectedRows.push(...filteredRows);
-    });
-  
-    return selectedRows;
-  };
-  
-  const filteredRowsWithOperator = (rows, columnFilters) => {
-    const filters = columnFilters.split(/ OR | or /);
-    const selectedRows = [];
-  
-    filters.forEach((filter) => {
-      const conditions = filter.split(/ AND | and /);
-      let conditionMatches = [];
-  
-      conditions.forEach((condition) => {
-        const [columnName, filterValue] = condition
-          .replace(/'/g, '')
-          .split(/[><=]/);
-        const operator = condition.replace(/[^><=]/g, '').trim();
-        const trimmedColumnName = columnName.trim().toLowerCase();
-        console.log(columnValue, ':::column value ');
-        const trimmedRowValue = filterValue.trim().toLowerCase();
-  
-        const filteredRows = rows.filter((row) => {
-          const columnValue = parseFloat(
-            row[trimmedColumnName].replace(/[^0-9.-]+/g, '')
-          );
-          if (!isNaN(columnValue)) {
-            switch (operator) {
-              case '>':
-                return columnValue > parseFloat(trimmedRowValue);
-              case '>=':
-                return columnValue >= parseFloat(trimmedRowValue);
-              case '<':
-                return columnValue < parseFloat(trimmedRowValue);
-              case '<=':
-                return columnValue <= parseFloat(trimmedRowValue);
-              case '=':
-                return columnValue === parseFloat(trimmedRowValue);
-              default:
-                return false;
-            }
+        const columnValue = parseFloat(
+          row[trimmedColumnName].replace(/[^0-9.-]+/g, '')
+        );
+        if (!isNaN(columnValue)) {
+          switch (operator) {
+            case '>':
+              return columnValue > parseFloat(trimmedRowValue);
+            case '>=':
+              return columnValue >= parseFloat(trimmedRowValue);
+            case '<':
+              return columnValue < parseFloat(trimmedRowValue);
+            case '<=':
+              return columnValue <= parseFloat(trimmedRowValue);
+            case '=':
+              return columnValue === parseFloat(trimmedRowValue);
+            default:
+              return false;
           }
-          return false;
-        });
-  
-        conditionMatches.push(filteredRows);
+        }
+        return false;
       });
-  
-      // Perform logical AND operation between condition matches
-      let andMatches = conditionMatches.shift() || [];
-      conditionMatches.forEach((conditionRows) => {
-        andMatches = andMatches.filter((row) => conditionRows.includes(row));
-      });
-  
-      selectedRows.push(...andMatches);
+
+      conditionMatches.push(filteredRows);
     });
-  
-    return selectedRows;
-  };
+
+    // Perform logical AND operation between condition matches
+    let andMatches = conditionMatches.shift() || [];
+    conditionMatches.forEach((conditionRows) => {
+      andMatches = andMatches.filter((row) => conditionRows.includes(row));
+    });
+
+    selectedRows.push(...andMatches);
+  });
+
+  return selectedRows;
+};
--- pdfToText/service/Function.js
+++ pdfToText/service/Function.js
@@ -1,71 +1,54 @@
 export const functions = [
-    {
-      name: 'row_to_select',
-      description: 'Select the rows based on given conditions',
-      parameters: {
-        type: 'object',
-        properties: {
-          columnFilters: {
-            type: 'string',
-            description: 'Filters to select rows based on conditions',
-          },
+  {
+    name: 'row_to_select',
+    description: 'Select the rows based on given conditions',
+    parameters: {
+      type: 'object',
+      properties: {
+        columnFilters: {
+          type: 'string',
+          description: 'Filters to select rows based on conditions',
         },
-        required: ['columnFilters'],
       },
+      required: ['columnFilters'],
     },
-    {
-      name: 'sum_of_column',
-      description: 'Calculate the sum of values in a specific column based on given conditions',
-      parameters: {
-        type: 'object',
-        properties: {
-          columnName: { 
-            type: 'string',
-            description: 'Name of the column for which to calculate the sum',
-          },
-          columnFilters: {
-            type: 'string',
-            description: 'Filters to select rows based on conditions',
-          },
+  },
+  {
+    name: 'sum_of_column',
+    description:
+      'Calculate the sum of values in a specific column based on given conditions',
+    parameters: {
+      type: 'object',
+      properties: {
+        columnName: {
+          type: 'string',
+          description: 'Name of the column for which to calculate the sum',
         },
-        required: ['columnName'],
-      },
-    },
-    {
-      name: 'average_of_column',
-      description: 'Calculate the average of values in a specific column based on given conditions',
-      parameters: {
-        type: 'object',
-        properties: {
-          columnName: { 
-            type: 'string',
-            description: 'Name of the column for which to calculate the average',
-          },
-          columnFilters: {
-            type: 'string',
-            description: 'Filters to select rows based on conditions',
-          },
+        columnFilters: {
+          type: 'string',
+          description: 'Filters to select rows based on conditions',
         },
-        required: ['columnName'], 
       },
+      required: ['columnName'],
     },
-    {
-      name: "sort_and_select",
-      description: "Sort the table based on a specific column, and select rows based on given conditions.",
-      parameters: {
-        type: "object",
-        properties: {
-          columnName: {
-            type: "string",
-            description: "Name of the column by which to sort the table."
-          },
-          columnFilters: {
-            type: "string",
-            description: "Filters to select rows based on conditions."
-          }
+  },
+  {
+    name: 'average_of_column',
+    description:
+      'Calculate the average of values in a specific column based on given conditions',
+    parameters: {
+      type: 'object',
+      properties: {
+        columnName: {
+          type: 'string',
+          description: 'Name of the column for which to calculate the average',
         },
-        required: ["columnName"], 
+        columnFilters: {
+          type: 'string',
+          description: 'Filters to select rows based on conditions',
+        },
       },
+      required: ['columnName'],
     },
-  ];
-  
+  },
+];
