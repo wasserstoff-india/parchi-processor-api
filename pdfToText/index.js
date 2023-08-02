d7459f0c48caa9d831a7d8809123745df1089424
ayush <tripathiayush600@gmail.com>
2023-08-02 15:16:08 +0530

    change

--- pdfToText/index.js
+++ pdfToText/index.js
@@ -9,7 +9,11 @@ import session from 'express-session';
 const app = express();
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors({ credentials: true }));
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 
 app.use(
   session({
