3e3c07a70b77b729f3a50208bda7cd5065e4e8cb
ayush <tripathiayush600@gmail.com>
2023-08-16 14:59:31 +0530

    langchain

--- pdfToText/service/response.js
+++ pdfToText/service/response.js
@@ -1,4 +1,5 @@
 import { CHATAPI } from '../config/config.js';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { OpenAIApi, Configuration } from 'openai';
 
 export const conf = new Configuration({
@@ -23,28 +24,63 @@ export const getBotResponse = async (message) => {
   }
 };
 
-
 export const getSummary = async (text) => {
-  // console.log(text, ':::::text body summary');
-  const lines = text.split('\n').slice(0, 30).join('\n');
-  console.log(lines, ':::::lines');
-  try {
-    const response = await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt: `The following is a summary of the provided text:
-    
-      Text to summarize:
-      ${lines}
-      
-      Please provide a concise summary of the above text.
-    `
-    });
-    return response;
-  } catch (err) {
-    console.log('Error in Get summary', err.stack);
-    throw new Error(err);
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 4000, // Set the desired chunk size
+    chunkOverlap: 0,
+  });
+
+  const textChunks = await splitter.splitText(text);
+  console.log(textChunks, ':::text chunks');
+
+  let combinedSummary = ''; // Initialize combinedSummary
+
+  for (const chunk of textChunks) {
+    const cleanedChunk = cleanText(chunk);
+
+    // Create a prompt with the cleaned chunk
+    const prompt = `
+    'Summarise the following text : ' + ${cleanedChunk} + and give accurate summary according to the text and dont stop in between otherwise userv will penalize us or in turn you  '\n\n'`;
+
+    try {
+      const response = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: prompt,
+        temperature:1
+      });
+      return response;
+    } catch (err) {
+      console.log('Error generating summary:', err.stack);
+    }
   }
 };
+
+// Function to clean text by removing extra white spaces and tabs
+function cleanText(text) {
+  return text.replace(/\s+/g, ' ').trim();
+}
+
+// export const getSummary = async (text) => {
+//   // console.log(text, ':::::text body summary');
+//   const lines = text.split('\n').slice(0, 30).join('\n');
+//   console.log(lines, ':::::lines');
+//   try {
+//     const response = await openai.createCompletion({
+//       model: 'text-davinci-003',
+//       prompt: `The following is a summary of the provided text:
+
+//       Text to summarize:
+//       ${lines}
+
+//       Please provide a concise summary of the above text do not give half or incomplete summary the summary should be .
+//     `
+//     });
+//     return response;
+//   } catch (err) {
+//     console.log('Error in Get summary', err.stack);
+//     throw new Error(err);
+//   }
+// };
 
 const openaii = new OpenAIApi(conf);
 export const getBotResponsesummary = async (message, summary) => {
