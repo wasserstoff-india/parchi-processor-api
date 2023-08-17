9b102270724021b15d491a879e76e97da715b6fd
ayush <tripathiayush600@gmail.com>
2023-08-17 13:42:01 +0530

    change

--- pdfToText/index.js
+++ pdfToText/index.js
@@ -10,11 +10,13 @@ const app = express();
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-app.use(
-  cors({
-    origin: '*',
-  })
-);
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 
 app.use(
   session({
